91490dbcf34dab60982fa4a4c7b091bf6cb5e7d2
Grant Ficklin <grant.ficklin@okstate.edu>
2020-04-24 19:07:05 -0500

    Created courses in dropdown

--- app/src/main/java/com/globomantics/cayagolf/Scores.java
+++ app/src/main/java/com/globomantics/cayagolf/Scores.java
@@ -1,24 +1,34 @@
 package com.globomantics.cayagolf;
-
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Scores extends AppCompatActivity {
     Spinner CourseSelection;
     RadioButton radioBlack, radioBlue, radioWhite, radioOrange, radio18, radioFront9, radioBack9;
     Button buttonNext;
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_scores);
-
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("AndroidView");
+        Spinner CourseSelection = findViewById(R.id.CourseSelection);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        CourseSelection.setAdapter(adapter);
         CourseSelection = findViewById(R.id.CourseSelection);
         radioBlack = findViewById(R.id.radioBlack);
         radioBlue = findViewById(R.id.radioBlue);
@@ -28,14 +38,15 @@ public class Scores extends AppCompatActivity {
         radioFront9 = findViewById(R.id.radioFront9);
         radioBack9 = findViewById(R.id.radioBack9);
 
+        final Spinner finalCourseSelection = CourseSelection;
         buttonNext.setOnClickListener(new View.OnClickListener() { @Override
         public void onClick(View v){
-            String course = CourseSelection.getSelectedItem().toString().trim();
+            String course = finalCourseSelection.getSelectedItem().toString().trim();
             String holes = radio18.toString().trim();
             String teeBox = radioBlack.toString().trim();
 
             if (TextUtils.isEmpty(course)) {
-                CourseSelection.setPrompt("Please Select a Course");
+                finalCourseSelection.setPrompt("Please Select a Course");
                 return;
             }
 
@@ -44,8 +55,17 @@ public class Scores extends AppCompatActivity {
             }
 
 
-        };
+        }
 
         });
+    }
+    private void setAdapter(ArrayAdapter<CharSequence> adapter) {
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.spinner, menu);
+        return true;
     }
 }
